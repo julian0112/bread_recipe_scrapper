7509abb54a8414260ea2092b7b988daa757eabd1
Julian <julian.lsanchez01@gmail.com>
2024-02-02 18:45:06 -0600

    Change loop so it goes based on the amount of recipes in the web page, also make the scrape run headless

--- main.py
+++ main.py
@@ -1,64 +1,92 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
-import requests
 import time 
+import pandas as pd
+import random
 
 #STARTS SELENIUM
 service = Service(executable_path="geckodriver.exe")
 
+#Selenium options for the browser
 options = webdriver.FirefoxOptions()
-#options.add_argument("-headless")
+options.add_argument("-headless")
 
 driver = webdriver.Firefox(options=options, service=service)
-driver.get('https://lacocinalatina.club/recetas/metodo/horno/')
 
-#It closes the pop up in the "Cocina Latina" web page
-close_popup = driver.find_element(By.CLASS_NAME, 'mc-closeModal').click()
-#It saves the "Cocina Latina" source page
-cocina_latina_source = driver.page_source
+#Dictionary to store data and use it to create a data frame and cvs file with pandas
+data = {
+    'titles': [],
+    'times': [],
+    'makes': [],
+    'ingredients': [],
+    'links': []
+}
+
 
 #STARTS BEAUTIFUL SOUP
-soup = BeautifulSoup(cocina_latina_source, 'lxml')
-
-#It searches for the "Cocina Latina" recipes
-recipes = soup.find_all('span', class_ = 'cooked-recipe-card-content')
-
-#A loop to extract the information of each recipe
-for recipe in recipes:
+#For loop to repeat the actions based on the amount of recipes
+counter = 1
+while True:
     
-    title = recipe.find('a').text
+    try:
     
-    #Checks if the title contains the word "Pan"
-    if 'Pan' in title:
+        #Start page with all the recipes buttons
+        driver.get('https://www.tasteofhome.com/collection/recipes-for-homemade-bread/')
+
+        #Check if the recipe button exists and then click it
+        wait = WebDriverWait(driver, 5)
+        bread = wait.until(EC.presence_of_element_located((By.XPATH, ('(//div[@class="listicle-page__cta-button"])['+ str(counter) + ']'))))
+        bread.click()
+
+        #Gets the recipe URL and then use it as the source for beautiful soup to search the info
+        driver.get(driver.current_url)
+        taste_of_home_source = driver.page_source
+
+        soup = BeautifulSoup(taste_of_home_source, 'lxml')
+
+        #Gets the title, total time, amount make, ingredients and link of the recipe
+        recipe_title = soup.find('h1', class_ = 'recipe-title').text
+        recipe_total_time = soup.find('div', class_ = 'total-time').p.text
+        recipe_makes = soup.find('div', class_ = 'makes').p.text
+
+        all_ingredients = soup.find('ul', class_ = 'recipe-ingredients__list recipe-ingredients__collection splitColumns')
+        recipe_ingredients = []
+
+        for ingredient in all_ingredients.find_all('li'):
+            recipe_ingredients.append(ingredient.text)
+            
+        recipe_link = driver.current_url
+
+        #It adds recipe to data dictionary
+        data['titles'].append(recipe_title)
+        data['times'].append(recipe_total_time)
+        data['makes'].append(recipe_makes)
+        data['ingredients'].append(recipe_ingredients)
+        data['links'].append(recipe_link)
+
+        print(counter+") "+recipe_title)
         
-        #QUE TOME ANTES DE ESTO EL NOMBRE DEL AUTOR
-        
-        #It search with selenium the hyperlink with the title in the text
-        #Then it runs a script to scroll down into view of the element
-        #The it clicks on the element to go into the respective directory
-        more_info = driver.find_element(By.PARTIAL_LINK_TEXT, title)
-        driver.execute_script("arguments[0].scrollIntoView();", more_info)
-        more_info.click()
-        
-        #BEAUTIFUL SOUP O SELENIUM PARA BUSCAR LOS INGREDIENTES Y LA DURACIÓN
-        
-        
-        time.sleep(4)
+        #Add to the counter 
+        counter+=1
+
         driver.back()
-        time.sleep(3)
-        
-        # bread_url = driver.current_url
-        # bread_page = BeautifulSoup(bread_url, 'lxml')
-        
-        # total_time = bread_page.find('span', class_ = 'cooked-total-time cooked-time')
-        # print(total_time)
-        
-        
-        
+        time.sleep(random.randint(1, 5))
+    
+    except TimeoutException:
+        #If the 5 second wait to find the button passes, it means that the button doesn't exist and it breaks the loop
+        break
+    
+    except Exception as error:
+        print("An exception occurred:", type(error).__name__)
+    
+# #Create data frame with the dictionary and uses it to create a csv file with the data 
+df = pd.DataFrame(data)
+df.to_csv('scraped_data.csv', index=False)
 
 
-# driver.quit()
+driver.quit()
