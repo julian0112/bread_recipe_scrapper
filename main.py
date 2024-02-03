d6108f20a3b3e0a4d270f09bbf0570b23ece2fc3
Julian <julian.lsanchez01@gmail.com>
2024-02-02 18:53:40 -0600

    Change loop so it goes based on the amount of recipes

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
-<<<<<<< HEAD
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-=======
->>>>>>> 8f333143de2d2be0faebbe552b1afbe26c9d2073
 from bs4 import BeautifulSoup
 import time 
 import pandas as pd
@@ -29,8 +26,6 @@ data = {
     'ingredients': [],
     'links': []
 }
-<<<<<<< HEAD
-
 
 #STARTS BEAUTIFUL SOUP
 #For loop to repeat the actions based on the amount of recipes
@@ -87,58 +82,9 @@ while True:
     
     except Exception as error:
         print("An exception occurred:", type(error).__name__)
-=======
-
-
-# #STARTS BEAUTIFUL SOUP
-
-#For loop to repeat the actions an x number of times, might be change so it ends when reaches the last recipe
-for x in range(5):
-    
-    driver.get('https://www.tasteofhome.com/collection/recipes-for-homemade-bread/')
-    
-    bread = driver.find_element(By.XPATH, ('(//div[@class="listicle-page__cta-button"])['+ str(x+1) + ']'))
-    bread.click()
-    
-    driver.get(driver.current_url)
-    taste_of_home_source = driver.page_source
-
-    soup = BeautifulSoup(taste_of_home_source, 'lxml')
-    
-    #Gets the title, total time, amount make, ingredients and link of the recipe
-    recipe_title = soup.find('h1', class_ = 'recipe-title').text
-    recipe_total_time = soup.find('div', class_ = 'total-time').p.text
-    recipe_makes = soup.find('div', class_ = 'makes').p.text
-
-    all_ingredients = soup.find('ul', class_ = 'recipe-ingredients__list recipe-ingredients__collection splitColumns')
-    recipe_ingredients = []
-
-    for ingredient in all_ingredients.find_all('li'):
-        recipe_ingredients.append(ingredient.text)
-        
-    recipe_link = driver.current_url
-    
-    #It adds recipe to data dictionary
-    data['titles'].append(recipe_title)
-    data['times'].append(recipe_total_time)
-    data['makes'].append(recipe_makes)
-    data['ingredients'].append(recipe_ingredients)
-    data['links'].append(recipe_link)
-
-    print(recipe_title)
-
-    driver.back()
-    time.sleep(random.randint(1, 3))
-    
->>>>>>> 8f333143de2d2be0faebbe552b1afbe26c9d2073
     
 # #Create data frame with the dictionary and uses it to create a csv file with the data 
 df = pd.DataFrame(data)
 df.to_csv('scraped_data.csv', index=False)
 
-
-<<<<<<< HEAD
 driver.quit()
-=======
-driver.quit()
->>>>>>> 8f333143de2d2be0faebbe552b1afbe26c9d2073
